33903435a4a359b922297cf3eddcb412935d0cca
Anton Bacaj <abacaj1@gmail.com>
2014-05-01 03:21:27 -0400

    commit

--- out/js/custom.js
+++ out/js/custom.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
-    $("a.scroll").click(function() {
+    $("a.scroll").click(function(e) {
+        e.preventDefault();
       $('html, body').animate({
         scrollTop: $("#folio").offset().top
       }, 450);
     });
+    
+    $(function(){
+		$('#menu').slicknav();
+	});
 });
